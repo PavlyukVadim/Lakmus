baf0c19b368b81081ca7e4f81aba7eb910bbac58
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-07 21:13:47 +0300

    add handlebar for change baseURL

--- brunch-config.js
+++ brunch-config.js
@@ -1,13 +1,25 @@
 // See http://brunch.io for documentation.
+const gitHubPageName = 'lakmus';
+
 exports.files = {
   javascripts: {
     joinTo: 'app.js'
   },
-  stylesheets: {joinTo: 'app.css'}
+  stylesheets: {
+    joinTo: 'app.css'
+  }
 };
 
 exports.plugins = {
-  babel: {presets: ['latest', 'react']}
+  babel: {
+    presets: ['latest', 'react']
+  },
+  handlebars: {
+    locals: {
+      title: 'Lakmus',
+      baseUrl: process.env.NODE_ENV === 'production' ? `/${gitHubPageName}.github.io` : ''
+    }
+  }
 };
 
 exports.hot = true;
