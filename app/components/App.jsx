f5ec28c401dcc7e846a8e870acb0e5694c2fbce8
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-08 17:31:10 +0300

    add Footer component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 export default class App extends Component {
   render() {
     return (
       <div id="content">
         <Header />
+        <main>
+          
+        </main>
+        <Footer />
       </div>
     );
   }
