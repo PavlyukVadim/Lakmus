cab6122023b4aadced3257155eddc453f51bfdb6
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-08 19:24:43 +0300

    rename exts

--- app/components/App.js
+++ app/components/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+import { DatePicker } from 'antd';
+
 export default class App extends Component {
   render() {
     return (
       <div id="content">
         <Header />
         <main>
-          
+          <DatePicker/>
         </main>
         <Footer />
       </div>
