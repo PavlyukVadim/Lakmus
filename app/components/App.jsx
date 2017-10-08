34373d305b3a3cab79371f07bb9c82d5cdcef28c
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-08 17:12:39 +0300

    add Header component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Counter from './Counter';
+import React, { Component } from 'react';
+import Header from './Header';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <div id="content">
-        <Counter />
+        <Header />
       </div>
     );
   }
