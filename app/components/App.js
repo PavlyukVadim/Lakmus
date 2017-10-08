8cfb06d3e7c646db5d20e8cbd2a6473fb9287e1a
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-08 22:13:27 +0300

    add test table from antd

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 import Header from './Header';
 import Footer from './Footer';
 
-import { DatePicker } from 'antd';
+const source = {
+  key: '1',
+  name: 'Mike',
+  phone: '+0 000 00 00 000',
+  email: 'some.email@some.some',
+  birthday: '01.01.1970',
+  address: '10 Downing Street',
+}
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: 'Phone',
+  dataIndex: 'phone',
+  key: 'phone',
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+  key: 'email',
+}, {
+  title: 'Birthday',
+  dataIndex: 'birthday',
+  key: 'birthday',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}];
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const dataSource = [];
+    let i = 0;
+    for (; i < 20; i++) {
+      dataSource[i] = Object.assign(source);
+      dataSource[i].key = dataSource.lenght;
+    } 
+    this.state = {
+      dataSource,
+    }
+  }
+
   render() {
     return (
       <div id="content">
         <Header />
         <main>
-          <DatePicker/>
+          <Table dataSource={this.state.dataSource} columns={columns} />
         </main>
         <Footer />
       </div>
--- app/initialize.js
+++ app/initialize.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import counterApp from './reducers';
-import App from 'components/App';
+import App from './components/App';
 
 const store = createStore(counterApp);
 
